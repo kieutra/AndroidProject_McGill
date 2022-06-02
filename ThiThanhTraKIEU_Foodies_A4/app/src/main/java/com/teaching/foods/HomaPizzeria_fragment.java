3e30ae890e6426f453ef2e8a2346cbd5e24324f7
kieutra <kieutra@gmail.com>
2022-06-02 19:34:43 -0400

    foodie project

---
+++ ThiThanhTraKIEU_Foodies_A4/app/src/main/java/com/teaching/foods/HomaPizzeria_fragment.java
@@ -0,0 +1,21 @@
+package com.teaching.foods;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class HomaPizzeria_fragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_homa_pizzeria_fragment, container, false);
+        return v;
+    }
+}
